fd7786c962cefe9c1c0322ed3bf7865b95ad5262
Stephen <stephencooper11395@gmail.com>
2024-07-15 17:17:14 -0500

    had two const using sequelize and it was interfering with code

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
-
-// Import environment variables
-require('dotenv').config();
+const sequelize = require('./config/connection'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,27 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// Configure Sequelize
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres'
-});
 
-// Test the database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-// Sync Sequelize models to the database
 sequelize.sync({ force: false }).then(() => {
   console.log('All models were synchronized successfully.');
   
-  // Start the server
+ 
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
